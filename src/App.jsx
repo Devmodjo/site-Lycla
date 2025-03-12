53ff29f66545f5b83a019cf8cb79be2502061c0e
Devmodjo <yvankamsu88@gmail.com>
2025-03-12 01:35:36 +0100

    configuration de la barre de navigation

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,7 @@ import "./style/theme.css";
 
 function App() {
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 50 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.8 }}
-      viewport={{ once: true }}
-      className="container my-5"
-    >
-      <h2 className="text-center">Titre de la Section</h2>
-      <p className="text-muted">Ce texte apparaît progressivement au scroll.</p>
-    </motion.div>
+    <Navbar/>
   );
 }
 
