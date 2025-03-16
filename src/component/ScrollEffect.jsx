af97b65425f3d592d7ace1ec6d505a9917f1921c
Devmodjo <yvankamsu88@gmail.com>
2025-03-16 10:23:21 +0100

    configation de l'effet de scroll

--- src/component/ScrollEffect.jsx
+++ src/component/ScrollEffect.jsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 export function FadeInSection({ children, delay = 0  }) {
   return (
@@ -24,4 +25,23 @@ export function SubTitleFadeIn({ children, delay = 0 }){
       {children}
     </motion.div>
   )
+}
+
+
+export function AppearOnScroll({ children, threshold=0.3 }) {
+  const [ref, inView] = useInView({
+    triggerOnce: true, // l'animation ce joue un seule fois
+    threshold: threshold, // l'animation se declenche a {threshold} de la hauteur de l'element
+  });
+
+  return (
+    <motion.div
+      ref={ref}
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 50 }}
+      transition={{ duration: 1 }}
+    >
+      {children}
+    </motion.div>
+  );
 }
